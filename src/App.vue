a2efaf722c29679ec8430b1ef628d8f7f9778667
Alberto Santiago <alberto.santiago@lkmx.io>
2021-08-26 06:30:09 -0700

    update work

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <h1>Gritape.js Workshop</h1>
+
+    <p>The token info</p>
+
+    <p>{{ tokenInfo }}</p>
+    
+    <div>
+      <p>Your count is: {{count}}</p>
+      <button @click="increment()">Increment</button>
+
+    </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { griptape } from '@stakeordie/griptape.js';
+import { stkd } from './contracts'
+import { secretCounter } from './contracts/secret-counter'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      tokenInfo: undefined,
+      count: 0
+    }
+  },
+  mounted(){
+    griptape.onConnect(() => {
+        this.fetchTokenInfo()
+        this.getCount();
+    });
+  },
+  methods:{
+    async fetchTokenInfo(){
+      const res = await stkd.getTokenInfo()
+      this.tokenInfo = res.token_info
+    },
+    async increment(){
+      await secretCounter.doIncrement();
+      await this.getCount();
+    },
+    async getCount(){
+      const res = await secretCounter.getCount();
+      this.count = res.count;
+    }
   }
+
 }
+
 </script>
 
 <style>
